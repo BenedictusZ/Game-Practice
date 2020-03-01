9bd128606ae6ccffdf1f8314f360b9fdc95c1376
tom <mozheng8@live.com>
2020-03-01 10:00:59 +0800

    create

---
+++ cmake-build-debug/CMakeFiles/1A2B.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1A2B.exe"
+  "1A2B.exe.manifest"
+  "1A2B.pdb"
+  "CMakeFiles/1A2B.dir/main.cpp.obj"
+  "lib1A2B.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1A2B.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/1A2B.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 1A2B.
+# This may be replaced when dependencies are built.
